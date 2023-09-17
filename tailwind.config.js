c384cc48e5746ca9fee6db9f23cf1f6b79ac76ae
Yutthana Tantinimitchok <tan@inspectrealestate.com.au>
2023-09-18 09:20:40 +1000

    add tailwind element

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,26 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//     content: ["./public/**/*.{html,js}"],
+//     theme: {
+//       extend: {
+//         colors:{
+//           primary:'#FF6363',
+//           secondary:{
+//              100:'#E2E2D5',
+//              200:'#888883'
+//         }
+//         }
+//       },
+//     },
+//     plugins: [],
+//   }
+
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-    content: ["./public/*.{html,js}"],
+  module.exports = {
+    content: [
+      "/public/**/*.{html,js}",
+      "./node_modules/tw-elements/dist/js/**/*.js"
+    ],
     theme: {
       extend: {
         colors:{
@@ -12,5 +32,6 @@ module.exports = {
         }
       },
     },
-    plugins: [],
-  }
+    plugins: [require("tw-elements/dist/plugin.cjs")],
+    darkMode: "class"
+  };
