d8b10edfeb7ce35de90f671dcca72dc53d2d7e9e
Yutthana Tantinimitchok <tan@inspectrealestate.com.au>
2023-09-19 02:48:10 +1000

    add detail for index.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,37 +1,20 @@
 // /** @type {import('tailwindcss').Config} */
 // module.exports = {
-//     content: ["./public/**/*.{html,js}"],
-//     theme: {
-//       extend: {
-//         colors:{
-//           primary:'#FF6363',
-//           secondary:{
-//              100:'#E2E2D5',
-//              200:'#888883'
-//         }
-//         }
-//       },
-//     },
-//     plugins: [],
-//   }
-
+//   content: ["./dist/*.{html,js}"],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
 /** @type {import('tailwindcss').Config} */
-  module.exports = {
-    content: [
-      "/public/**/*.{html,js}",
-      "./node_modules/tw-elements/dist/js/**/*.js"
-    ],
-    theme: {
-      extend: {
-        colors:{
-          primary:'#FF6363',
-          secondary:{
-             100:'#E2E2D5',
-             200:'#888883'
-        }
-        }
-      },
-    },
-    plugins: [require("tw-elements/dist/plugin.cjs")],
-    darkMode: "class"
-  };
+module.exports = {
+  content: [
+    "./dist/*.{html,js}",
+    "./node_modules/tw-elements/dist/js/**/*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("tw-elements/dist/plugin.cjs")],
+  darkMode: "class"
+};
